2ddacb546b0b194fee595c8dee8dcd5be48fb0d4
KanashiXI <panyapat.test@gmail.com>
2019-10-20 09:18:19 +0700

    tab

--- application/views/activity/content.php
+++ application/views/activity/content.php
@@ -15,24 +15,24 @@
 
   <div class="row">
     <div class="col-md-12">
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">ชื่อกิจกรรม</th>
-          <th scope="col">วันที่ทำกิจกรรม</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>กิจกรรมค่าย 4 ชั้นปี</td>
-          <td>12 เม.ย. - 15 เม.ย. 2563</td>
-          <td><a href="<?php echo base_url('index.php/activity/update'); ?>" type="button" class="btn btn-warning">แก้ไข</a>
-            <a href="<?php echo base_url('index.php/activity/delete'); ?>" type="button" class="btn btn-danger">ลบ</a></td>
-        </tr>
-      </tbody>
-    </table>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">ชื่อกิจกรรม</th>
+            <th scope="col">วันที่ทำกิจกรรม</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>กิจกรรมค่าย 4 ชั้นปี</td>
+            <td>12 เม.ย. - 15 เม.ย. 2563</td>
+            <td><a href="<?php echo base_url('index.php/activity/update'); ?>" type="button" class="btn btn-warning">แก้ไข</a>
+              <a href="<?php echo base_url('index.php/activity/delete'); ?>" type="button" class="btn btn-danger">ลบ</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
-</div>
