f2b198e33f319d8b69bdd8ae44312a86981d5f60
KanashiXI <panyapat.test@gmail.com>
2019-10-20 10:11:07 +0700

    hide url index.php

--- application/views/activity/content.php
+++ application/views/activity/content.php
@@ -7,7 +7,7 @@
         <input type="text" class="form-control" placeholder="ชื่อกิจกรรม" aria-label="Recipient's username" aria-describedby="basic-addon2">
         <div class="input-group-append">
           <button class="btn btn-outline-secondary" type="button">ค้นหา</button>
-          <a href="<?php echo base_url('index.php/activity/insert'); ?>" class="btn btn-info" type="button">เพิ่มกิจกรรม</a>
+          <a href="<?php echo base_url('activity/insert'); ?>" class="btn btn-info" type="button">เพิ่มกิจกรรม</a>
         </div>
       </div>
     </div>
@@ -29,8 +29,8 @@
             <th scope="row">1</th>
             <td>กิจกรรมค่าย 4 ชั้นปี</td>
             <td>12 เม.ย. - 15 เม.ย. 2563</td>
-            <td><a href="<?php echo base_url('index.php/activity/update'); ?>" type="button" class="btn btn-warning">แก้ไข</a>
-              <a href="<?php echo base_url('index.php/activity/delete'); ?>" type="button" class="btn btn-danger">ลบ</a></td>
+            <td><a href="<?php echo base_url('activity/update'); ?>" type="button" class="btn btn-warning">แก้ไข</a>
+              <a href="<?php echo base_url('activity/delete'); ?>" type="button" class="btn btn-danger">ลบ</a></td>
           </tr>
         </tbody>
       </table>
--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -1,8 +1,8 @@
 <body>
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-  <h5 class="my-0 mr-md-auto font-weight-normal"><a href="<?php echo base_url('index.php/activity/'); ?>">ระบบกิจกรรม</a></h5>
+  <h5 class="my-0 mr-md-auto font-weight-normal"><a href="<?php echo base_url(''); ?>">ระบบกิจกรรม</a></h5>
   <nav class="my-2 my-md-0 mr-md-3 navbar-right">
-    <a class="p-2 text-dark" href="http://localhost/activities/index.php/activity/">กิจกรรม</a>
+    <a class="p-2 text-dark" href="<?php echo base_url('activity/') ?>">กิจกรรม</a>
     <a class="p-2 text-dark" href="#">Enterprise</a>
     <a class="p-2 text-dark" href="#">Support</a>
     <a class="p-2 text-dark" href="#">Pricing</a>
